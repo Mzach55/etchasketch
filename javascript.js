fac12171095909bcda0a601acdf7777207e0f712
Mzach55 <mikezach37@gmail.com>
2023-02-09 10:08:02 -0500

    fixed drag bug
    fixed dragbug and initial tile issue

--- javascript.js
+++ javascript.js
@@ -34,10 +34,15 @@ drawing.addEventListener('click', () => {
 
 // allows the placing of color depending on if pen is true or false!
 
-function placeColor (e) {
-    console.log(e)
+function placeColor (eventone) {
+    console.log(eventone)
+    if (pen){
+        eventone.target.style.background=`${changecolor.value}`
+    }
+
     draw.forEach(draw => draw.addEventListener('mouseover', function(e){
     if(e.buttons == 1 || e.buttons == 3){
+        console.log(e)
         e.target.style.background=`${changecolor.value}`;
         if (pen == false){
             e.target.style.background='white';
@@ -57,8 +62,15 @@ function singleColor (e){
 
 let pen = true;
 const draw = document.querySelectorAll('.gridbox')
-draw.forEach(draw => draw.addEventListener('click', singleColor))
+draw.forEach(draw => draw.addEventListener('click', singleColor));
 draw.forEach(draw => draw.addEventListener('mousedown', placeColor));
+
+function dragstart (event) {
+    event.preventDefault()
+}
+
+draw.forEach(draw => draw.addEventListener('mousedown', dragstart))
+
 
 const changecolor = document.querySelector('#colorPicker');
 
